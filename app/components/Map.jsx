a217fe7a48589dedb4955dd3ce52958ac970f3d0
solithcy <me@rose.gay>
2025-08-19 16:23:55 +0100

    fix zoom issues, add scale widget

--- app/components/Map.jsx
+++ app/components/Map.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import {LineLayer} from '@deck.gl/layers';
-import {useWidget, ZoomWidget} from '@deck.gl/react';
-import {Map, useControl} from 'react-map-gl/maplibre';
+// import {useWidget, ZoomWidget} from '@deck.gl/react';
+import {Map, useControl, NavigationControl, ScaleControl} from 'react-map-gl/maplibre';
 import {MapView, FirstPersonView} from '@deck.gl/core';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import data from './new.json';
 import {H3HexagonLayer} from '@deck.gl/geo-layers';
 import {getTweenedColorHsl} from "../util/color.js";
 import {MapboxOverlay} from '@deck.gl/mapbox';
-// import {ZoomWidget} from '@deck.gl/widgets';
+import {ZoomWidget} from '@deck.gl/widgets';
 import '@deck.gl/widgets/stylesheet.css';
 
 
 const INITIAL_VIEW_STATE = {
   longitude: -3.24718538,
-  latitude: 53.15126933, 
-  zoom: 5.93, 
-  bearing: -27, 
+  latitude: 53.15126933,
+  zoom: 5.93,
+  bearing: -27,
   pitch: 40.5,
 };
 
@@ -29,15 +29,15 @@ export const colorRange = [
   [209, 55, 78, 255],
 ];
 
-function DeckGLOverlay(props) {
-    const overlay = useControl(() => new MapboxOverlay(props));
-    overlay.setProps(props);
-    return null;
+function DeckGLOverlay(props){
+  const overlay = useControl(() => new MapboxOverlay(props));
+  overlay.setProps(props);
+  return null;
 }
 
-function ZoomWidgetComponent(props) {
-    const overlay = useWidget(ZoomWidget, props);
-    return null;
+function ZoomWidgetComponent(props){
+  const overlay = useControl(() => new ZoomWidget(props));
+  return null;
 }
 
 export default function MapComponent(){
@@ -48,7 +48,7 @@ export default function MapComponent(){
     pickable: true,
     getHexagon: d => d[0],
     getElevation: d => d[activeIdx],
-    getFillColor: d => getTweenedColorHsl(Math.min(1, Math.max(0, d[activeIdx] / 50)), colorRange),
+    getFillColor: d => getTweenedColorHsl(Math.min(1, Math.max(0, d[activeIdx] / 10)), colorRange),
     extruded: true,
     material: {
       ambient: 0.64,
@@ -64,37 +64,37 @@ export default function MapComponent(){
     coverage: 0.8,
     autoHighlight: true,
     highlightColor: [255, 255, 255, 100],
-    elevationScale: 2000,
+    elevationScale: 250,
 
     onClick: info => {
-      if (info.object) {
+      if(info.object){
         console.log('Clicked hexagon:', info.object);
       }
     }
   })
-  
 
-    return (
-        <Map 
-          style={{ width: '100%', height: '100vh' }}
-  
-        mapStyle="https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json"
-        initialViewState={{
-            longitude: -3.24718538, 
-            latitude: 53.15126933, 
-            zoom: 5.93, 
-            bearing: -27, 
-            pitch: 40.5,
-        }}
-        >
-            <DeckGLOverlay 
-              layers={[layer]} 
-              interleaved 
-            >
-              <ZoomWidgetComponent></ZoomWidgetComponent>
 
-              </DeckGLOverlay>
-
-        </Map>
-    )
+  return (
+    <Map
+      style={{width: '100%', height: '100vh'}}
+      mapStyle="https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json"
+      initialViewState={{
+        longitude: -3.24718538,
+        latitude: 53.15126933,
+        zoom: 5.93,
+        bearing: -27,
+        pitch: 40.5,
+      }}
+      minZoom={5}
+      maxZoom={15}
+      doubleClickZoom={false}
+    >
+      <DeckGLOverlay
+        layers={[layer]}
+        interleaved
+      />
+      <NavigationControl/>
+      <ScaleControl/>
+    </Map>
+  )
 }
