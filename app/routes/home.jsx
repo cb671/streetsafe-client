e7e57b63090f4420c2a5eeaf5686f4585bff96a2
solithcy <me@rose.gay>
2025-08-20 09:52:56 +0100

    add onclick

--- app/routes/home.jsx
+++ app/routes/home.jsx
@@ -1,12 +1,19 @@
-import { Welcome } from "../welcome/welcome.jsx";
-
-export function meta({}) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
-}
+import {useState} from "react";
+import Map from "../components/Map.jsx";
 
 export default function Home() {
-  return <Welcome />;
+  const [clicked, setClicked] = useState(null);
+  return (
+    <>
+      {
+        clicked && <div className="fixed bottom-0 left-0 w-[100vw] h-[50vh] bg-black/75 backdrop-blur-2xl z-10 rounded-t-2xl">
+          <h1 className={"text-center m-4 text-2xl font-bold"}>{clicked[0]} - h3 index</h1>
+        </div>
+      }
+
+      <Map onClick={e=>{
+        setClicked(e);
+      }}/>
+    </>
+  );
 }
