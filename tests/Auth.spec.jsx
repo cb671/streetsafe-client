f2323131ad03a25ecd5561a9e0c747c37b3f04c4
solithcy <me@rose.gay>
2025-08-26 17:31:08 +0100

    fix auth typing test not working

--- tests/Auth.spec.jsx
+++ tests/Auth.spec.jsx
@@ -14,29 +14,28 @@ const RegistrationStub = createRoutesStub([
 
 describe("Register component works", () => {
 
-    it("renders the registration form", () => {   
-        
-        const page = render(<body><RegistrationStub initialEntries={["/register"]}/></body>);
+    it("renders the registration form", () => {
+
+        const page = render(<RegistrationStub initialEntries={["/register"]}/>);
 
         const form = page.getByTestId("form");
 
         expect(form).toBeInTheDocument()
-        
+
     });
 
-    
+
     it("submits the form when the submit button is clicked", async () => {
 
         vi.mock('../app/api/api.js', {spy: true});
-        
-        const page = render(<body><RegistrationStub initialEntries={["/register"]} /></body>);
 
+        const page = render(<RegistrationStub initialEntries={["/register"]} />);
         const form = page.getByTestId("form");
 
-        await userEvent.type(document.querySelector('input[name=username]'), 'bob@bob.com')
-        await userEvent.type(document.querySelector('input[name=email]'), '   bob@bob.com')
-        await userEvent.type(document.querySelector('input[name=password]'), 'bob@bob.com')
-        await userEvent.type(document.querySelector('input[name=postcode]'), 'N16 5JJ')
+        await userEvent.fill(document.querySelector('input[name=username]'), 'bob@bob.com')
+        await userEvent.fill(document.querySelector('input[name=email]'), '   bob@bob.com')
+        await userEvent.fill(document.querySelector('input[name=password]'), 'bob@bob.com')
+        await userEvent.fill(document.querySelector('input[name=postcode]'), 'N16 5JJ')
 
         await userEvent.click(document.querySelector('button[type=submit]'))
 
