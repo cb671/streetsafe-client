6241098b7060df02f0d5eb8582625a751433c526
Gayathri Krishnan <gayu@Gayathris-MacBook-Air.local>
2025-08-27 11:00:50 +0100

    merge conflicit changes

--- app/api/api.js
+++ app/api/api.js
@@ -19,6 +19,7 @@ export function getBarChartData(filter) {
   return fetch(API_ROOT + '/graphs/totals?'+ search.toString()).then((r) =>
     r.json()
   );
+}
 export function getMapData(){
   return fetch(API_ROOT + '/map').then(r => r.json())
 }
@@ -60,4 +61,28 @@ export function reverseGeo(lon, lat){
     body: JSON.stringify([lon, lat]),
     credentials: "include"
   }).then(r => r.json()).catch(err => ({message: err.toString()}))
+}
+
+export function getPieChartData(filter) {
+  const search = filterParamsBuilder(filter);
+  return fetch(API_ROOT + '/graphs/proportions?' + search.toString()).then(
+    (r) => r.json()
+  );
+}
+function filterParamsBuilder({
+  startDate,
+  endDate,
+  location,
+  radius,
+  crimeTypes,
+}) {
+  const search = new URLSearchParams();
+  if (startDate) search.set('startDate', startDate);
+  if (endDate) search.set('endDate', endDate);
+  if (location) search.set('location', location);
+  if (radius) search.set('radius', radius);
+  if (crimeTypes && Array.isArray(crimeTypes) && crimeTypes.length > 0) {
+    crimeTypes.forEach((type) => search.append('crimeTypes', type));
+  }
+  return search;
 }
