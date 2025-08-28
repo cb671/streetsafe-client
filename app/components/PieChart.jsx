d2d7a1db9071dcf3e51783775c90df5aff7b940d
Solithcy <me@rose.gay>
2025-08-28 10:26:56 +0100

    make backgrounds less translucent

--- app/components/PieChart.jsx
+++ app/components/PieChart.jsx
@@ -29,12 +29,12 @@ const PieChart = ({filter}) => {
         label: 'Crime proportions',
         data: values,
         backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 99, 132, 0.4)',
+          'rgba(54, 162, 235, 0.4)',
+          'rgba(255, 206, 86, 0.4)',
+          'rgba(75, 192, 192, 0.4)',
+          'rgba(153, 102, 255, 0.4)',
+          'rgba(255, 159, 64, 0.4)',
         ],
         borderColor: [
           'rgba(255, 99, 132, 1)',
